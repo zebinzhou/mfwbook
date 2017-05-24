f4787c111bc33af2bdb0cbb0b3af3f2170755e26
Donlle Guo <guodongrui@outlook.com>
2017-05-24 10:34:05 +0800

    a simple website

---
+++ src/main/java/org/mfwbook/UserController.java
@@ -0,0 +1,22 @@
+package org.mfwbook;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class UserController {
+    @RequestMapping("/")
+    public String user(Model model) {
+        User user = new User("test", "testingnum");
+        List<User> list = new ArrayList<>();
+        list.add(user);
+        model.addAllAttributes(list);
+        
+        return "user";
+    }
+}
