a20225af5f8f143fd495f862ac367f6d8c037d3f
zebinzhou <zebinzhou@outlook.com>
2017-06-14 21:58:25 +0800

    delete modified

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,17 +1,17 @@
-#insert into USER (id, name, password) values (1, 'zhenxiongwu', '123');
-#insert into USER (id, name, password) values (2, 'guodongrui', '123');
-#
-#insert into SYS_ROLE (id,name) values (1,'ROLE_ADMIN');
-#insert into SYS_ROLE (id,name) values (2,'ROLE_USER');
-#
-#insert into book(book_id, book_name, author, publisher, book_types, read_times, search_times) values('9787121282089', 'JavaEE开发的颠覆者: Spring Boot实战', '汪云飞', '电子工业出版社', 'Java, 编程, 教育', 0, 0);
-#insert into book(book_id, book_name, author, publisher, book_types, read_times, search_times) values('9787115417305', 'Spring实战（第4版）', 'Craig Walls 沃尔斯', '人民邮电出版社', 'Java, 编程, 教育', 0, 0);
-#insert into book(book_id, book_name, author, publisher, book_types, read_times, search_times) values('9787115209429', 'Spring揭秘', '王福强', '人民邮电出版社', 'Java, 框架, Web开发', 0, 0);
-#insert into book(book_id, book_name, author, publisher, book_types, read_times, search_times) values('9787544242516', '白夜行', ' 东野圭吾', '南海出版公司', '推理, 日本, 悬疑, 小说', 0, 0);
-#insert into book(book_id, book_name, author, publisher, book_types, read_times, search_times) values('9787544270878', '解忧杂货店', ' 东野圭吾', '南海出版公司', '温暖, 日本, 治愈, 小说', 0, 0);
-#insert into book(book_id, book_name, author, publisher, book_types, read_times, search_times) values('9787538276374', '上帝掷骰子吗', '曹天元', '辽宁教育出版社', '科普, 量子物理, 物理, 科学史, 量子力学, 科学', 0, 0);
-#insert into book(book_id, book_name, author, publisher, book_types, read_times, search_times) values('9787206030048', '自私的基因', '卢允中', '吉林人民出版社', '科普, 基因, 进化论, 生物学, 科学', 0, 0);
-#insert into book(book_id, book_name, author, publisher, book_types, read_times, search_times) values('9787508036083', '国富论', '亚当·斯密', '华夏出版社', '经济学, 国富论, 亚当・斯密, 经济, 经典', 0, 0);
-#insert into book(book_id, book_name, author, publisher, book_types, read_times, search_times) values('9787508659794', '行动的勇气', '本·伯南克', '中信出版社', '金融, 经济, 金融危机, 传记', 0, 0);
-#insert into book(book_id, book_name, author, publisher, book_types, read_times, search_times) values('9787807463726', '艺术的故事', '贡布里希', '广西美术出版社', '艺术, 艺术史, 贡布里希, 艺术的故事, 历史, 文化, 经典', 0, 0);
-#insert into book(book_id, book_name, author, publisher, book_types, read_times, search_times) values('9787805638195', '美的历程', '李泽厚', '天津社会科学院出版社', '美学, 李泽厚, 艺术, 美的历程, 哲学, 文化', 0, 0);
+insert into USER (id, name, password) values (1, 'zhenxiongwu', '123');
+insert into USER (id, name, password) values (2, 'guodongrui', '123');
+
+insert into SYS_ROLE (id,name) values (1,'ROLE_ADMIN');
+insert into SYS_ROLE (id,name) values (2,'ROLE_USER');
+
+insert into book(book_id, book_name, author, publisher, book_types, read_times, search_times) values('9787121282089', 'JavaEE开发的颠覆者: Spring Boot实战', '汪云飞', '电子工业出版社', 'Java, 编程, 教育', 0, 0);
+insert into book(book_id, book_name, author, publisher, book_types, read_times, search_times) values('9787115417305', 'Spring实战（第4版）', 'Craig Walls 沃尔斯', '人民邮电出版社', 'Java, 编程, 教育', 0, 0);
+insert into book(book_id, book_name, author, publisher, book_types, read_times, search_times) values('9787115209429', 'Spring揭秘', '王福强', '人民邮电出版社', 'Java, 框架, Web开发', 0, 0);
+insert into book(book_id, book_name, author, publisher, book_types, read_times, search_times) values('9787544242516', '白夜行', ' 东野圭吾', '南海出版公司', '推理, 日本, 悬疑, 小说', 0, 0);
+insert into book(book_id, book_name, author, publisher, book_types, read_times, search_times) values('9787544270878', '解忧杂货店', ' 东野圭吾', '南海出版公司', '温暖, 日本, 治愈, 小说', 0, 0);
+insert into book(book_id, book_name, author, publisher, book_types, read_times, search_times) values('9787538276374', '上帝掷骰子吗', '曹天元', '辽宁教育出版社', '科普, 量子物理, 物理, 科学史, 量子力学, 科学', 0, 0);
+insert into book(book_id, book_name, author, publisher, book_types, read_times, search_times) values('9787206030048', '自私的基因', '卢允中', '吉林人民出版社', '科普, 基因, 进化论, 生物学, 科学', 0, 0);
+insert into book(book_id, book_name, author, publisher, book_types, read_times, search_times) values('9787508036083', '国富论', '亚当·斯密', '华夏出版社', '经济学, 国富论, 亚当・斯密, 经济, 经典', 0, 0);
+insert into book(book_id, book_name, author, publisher, book_types, read_times, search_times) values('9787508659794', '行动的勇气', '本·伯南克', '中信出版社', '金融, 经济, 金融危机, 传记', 0, 0);
+insert into book(book_id, book_name, author, publisher, book_types, read_times, search_times) values('9787807463726', '艺术的故事', '贡布里希', '广西美术出版社', '艺术, 艺术史, 贡布里希, 艺术的故事, 历史, 文化, 经典', 0, 0);
+insert into book(book_id, book_name, author, publisher, book_types, read_times, search_times) values('9787805638195', '美的历程', '李泽厚', '天津社会科学院出版社', '美学, 李泽厚, 艺术, 美的历程, 哲学, 文化', 0, 0);
